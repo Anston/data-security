190e4458e1fa1448ff96d156d0acaecde7d48608
sudhakarlarus <victrsudhakar@gmail.com>
2016-03-27 15:33:05 +0530

    Division module 1
    Added module 1

--- src/java/Division.java
+++ src/java/Division.java
@@ -6,6 +6,7 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +16,8 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Shawn
  */
-public class Division extends HttpServlet {
+public class Division extends HttpServlet 
+{
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -27,19 +29,67 @@ public class Division extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException
+    {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Division</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Division ghg at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        try (PrintWriter out = response.getWriter()) 
+        {
+            int[] a={1,4,9,16,25};
+            String passplit[] = new String[5];
+            int counter = 0;
+            int i,c,e,f,x;
+            String uname = request.getParameter("username");
+            String pass = request.getParameter("pass");
+            char Pass[]=pass.toCharArray();
+            int b = pass.length();
+         for(i=0;i<5;)
+	    {
+		if(a[i]>b)
+		{
+		    c=a[i];
+		    break;
+		}
+		else
+		{
+		i++;
+		}
+	    }
+		 int d;
+            d = a[i-1];
+          int m,n;
+          m= b-d;
+          n= c-b;
+          if(m<n)
+          {
+              e=d;
+          }
+          else
+          {
+              e=c;
+          }
+          
+          switch(e)
+          {
+              case 1:
+                  f=1;
+                  break;
+              case 4:
+                  f=2;
+                  break;
+              case 9:
+                  f=3;
+                  break;
+              case 16:
+                  f=4;
+                  break;
+              case 25:
+                  f=5;
+                  break;
+              default:
+                  break;
+                } 
+       for (int j = 0; i < b-f+1; i+= f)
+    passplit[counter++] = Arrays.copyOfRange(Pass[],j,j + f);
         }
     }
 
